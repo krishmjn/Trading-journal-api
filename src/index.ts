f6698e91629f20e8c069ab471acf09f071705678
krishmjn <maharjankrish51@gmail.com>
2025-11-26 08:14:48 +0545

    push config

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://trading-journal-ui.vercel.app"],
+  origin: [
+    "http://localhost:5173",
+    "https://trading-journal-ui.vercel.app",
+    "https://trading-journal-ui-git-main-krishmjns-projects.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
